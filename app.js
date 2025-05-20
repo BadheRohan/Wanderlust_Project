4a2a369a4d898235ff4d057cb62bc58ff46cd9a4
BadheRohan <rohanbadhe014@gmail.com>
2025-05-21 02:35:04 +0530

    edited files for Project

--- app.js
+++ app.js
@@ -95,6 +95,7 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(8080,'0.0.0.0', () => {
-    console.log("server is listening to port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is listening on port ${port}`);
 });
